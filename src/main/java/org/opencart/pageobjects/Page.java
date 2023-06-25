973b60ef69a3ae0a2efd4536d837f95674edbb41
Donic Vadim <vadimdonik710@gmail.com>
2023-06-25 22:03:11 +0300

    Application of page object model and page factory.

---
+++ src/main/java/org/opencart/pageobjects/Page.java
@@ -0,0 +1,27 @@
+package org.opencart.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class Page {
+
+    public Page(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//i[@class='fa-solid fa-user']")
+    protected WebElement accountIcon;
+
+    @FindBy(xpath = "//a[normalize-space()='Register']")
+    protected WebElement registerButton;
+
+    public void navigateToRegisterPageFromHeaderMenu() {
+        accountIcon.click();
+        System.out.println("The account icon was clicked ");
+
+        registerButton.click();
+        System.out.println("The register button was clicked ");
+    }
+}
